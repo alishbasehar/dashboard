de290977445c6348c3d1ecabb1c5361dc5594af2
alishbasehar <alishbasehar505@gmail.com>
2026-01-27 23:41:34 +0500

    updated files

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = () => {
+  const [expand ,setexpand]=useState(false)
   return (
     <div className='text-white'>card</div>
   )
